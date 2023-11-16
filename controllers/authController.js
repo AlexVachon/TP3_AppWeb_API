856e91a86d4f629d0d38a6bcd4a0623a987b9db5
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2023-11-16 15:18:17 -0500

    *login, *signup, +createToken

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,80 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const config = require('../config');
-const User = require('../models/user');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+const User = require("../models/user");
 
-exports.login = (req, res, next) => {
+function createToken(email) {
+  return jwt.sign({ email: email }, process.env.SECRET_JWT, {
+    expiresIn: "1h",
+  });
+}
+
+exports.login = async (req, res, next) => {
   const { email, password } = req.body;
+  // TODO: Ajouter les chemins pour les pages
+  try {
+    const user = await User.findOne({ email: email });
 
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Adresse e-mail ou mot de passe incorrect." });
+    }
+    if (await bcrypt.compare(password, user.password)) {
+      if (user.isValet) {
+        return res.status(200).json({
+          message: "Rediriger vers la page Valet",
+          token: createToken(email),
+        });
+      }
+      return res.status(200).json({
+        message: "Rediriger vers Ma place",
+        token: createToken(email),
+      });
+    }
 
+    return res
+      .status(401)
+      .json({ message: "Adresse e-mail ou mot de passe incorrect." });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
   const { email, username, password, confirmPassword } = req.body;
 
+  try {
+    if (password !== confirmPassword) {
+      return res
+        .status(401)
+        .json({ message: "Les mots de passe doivent être identiques!" });
+    }
 
+    const new_user = User({
+      email: email,
+      username: username,
+      password: password,
+    });
+
+    await new_user.save();
+    return res.status(200).json({
+      message: "Compte créé avec succès!",
+      token: createToken(new_user.email),
+    });
+  } catch (error) {
+    if (error instanceof Error.ValidationError) {
+      const validationErrors = {};
+
+      for (const key in error.errors) {
+        validationErrors[key] = error.errors[key].message;
+      }
+
+      return res.status(400).json({ errors: validationErrors });
+    } else {
+      console.error(error);
+      return res.status(500).json({ message: "Erreur interne du serveur" });
+    }
+  }
 };
-
