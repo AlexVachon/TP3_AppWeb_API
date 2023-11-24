605e6cc22f76192f3549d9b371777374bd4dc6d3
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2023-11-24 08:13:27 -0500

    *effectuerPaiement, *getFacture

--- controllers/histoController.js
+++ controllers/histoController.js
@@ -1,34 +1,63 @@
-const Histo = require('../models/historique');
-const Facture = require('../models/facture');
+const Histo = require("../models/historique");
+const Facture = require("../models/facture");
 
 exports.getHistorique = async (req, res, next) => {
   const userId = req.user.userId;
-  console.log('userId', userId)
+  console.log("userId", userId);
   try {
-    const histo = await Histo
-    .find({ userId: userId })
-    .sort({ createdAt: -1 });
+    const histo = await Histo.find({ userId: userId }).sort({ createdAt: -1 });
     if (!histo) {
-      const error = new Error('Aucun historique trouvé.');
+      const error = new Error("Aucun historique trouvé.");
       error.statusCode = 404;
       throw error;
     }
-    
+
     res.status(200).json({
-      histo: histo
+      histo: histo,
     });
   } catch (err) {
     next(err);
   }
-}
+};
 
 exports.effectuerPaiement = async (req, res, next) => {
   const userId = req.user.userId;
- 
-}
+  const { histoID } = req.body;
+
+  try {
+    const histo = await Histo.findById(histoID);
+    if (!histo) {
+      return res
+        .status(404)
+        .json({ message: "Aucun historique trouvé avec cet ID." });
+    }
+
+    const facture = new Facture({
+      userId: userId,
+      price: histo.price,
+    });
+
+    histo.isPaid = true;
+    await histo.save();
+    await facture.save();
+
+    return res.status(201).json({ message: "Facture payée!" });
+  } catch (error) {
+    next(error);
+  }
+};
 
 exports.getFacture = async (req, res, next) => {
   const userId = req.user.userId;
- 
-}
+  try {
+    const facture = await Facture.find({ userId: userId });
 
+    if (!facture) {
+      return res.status(404).json({ message: "Aucune facture à ce jour!" });
+    }
+
+    return res.status(201).json(facture);
+  } catch (error) {
+    next(error);
+  }
+};
