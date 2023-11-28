6a399499213251456ac912fae8c07a8356a18f22
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2023-11-28 10:41:40 -0500

    *getUser

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -57,7 +57,7 @@ exports.getUser = async (req, res, next) => {
 
 exports.getUserById = async (req, res, next) => {
   try {
-    const userId = req.params.userId;
+    const userId = req.query.userId
     
     if (!userId) {
       return res.status(400).json({ error: 'userId non spécifié dans la requête' });
